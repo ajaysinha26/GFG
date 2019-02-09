05b17e10b955bce2fffb123b3a741c27c38d5843
ajaysinha26 <ajaysinha26@gmail.com>
2019-02-09 17:40:31 +0530

    adding program description

--- src/array/NextSmallerElement.java
+++ src/array/NextSmallerElement.java
@@ -4,8 +4,10 @@ import java.util.Stack;
 
 /**
  * Created by ajaysinha on 09/02/19.
- * 
+ *
  * Program to find Next Smaller Element(NSE) in a given array.
+ * NSE of last element is -1. If NSE doesn't exist for an element NSE would be considered as -1
+ * For example if array is 4, 8, 5, 2, 25 then NSE would be 4 -> 2, 8 -> 5, 5 -> 2, 2 -> -1, 25 -> -1
  * This prints the NSE in reverse order of the array. If needed to print in order of array we can use an additional storage.
  */
 public class NextSmallerElement {
