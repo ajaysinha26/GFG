2fe6f92ed24c7f6e1cfb918495b9f4626ef2fbad
ajaysinha26 <ajaysinha26@gmail.com>
2019-02-09 17:16:45 +0530

    Next smaller element in array

--- src/array/NextSmallerElement.java
+++ src/array/NextSmallerElement.java
@@ -1,11 +1,28 @@
 package array;
 
+import java.util.Stack;
+
 /**
  * Created by ajaysinha on 09/02/19.
  */
 public class NextSmallerElement {
 
     public static void main(String[] args) {
+        int[] array = {4, 8, 5, 2, 25};
+//        int[] array = { 11, 13, 21, 3};
 
+
+        Stack<Integer> stack = new Stack<>();
+        for(int i = array.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() > array[i]) {
+                stack.pop();
+            }
+            if(stack.isEmpty()) {
+                System.out.println("NSE of " + array[i] + " is -1");
+            } else {
+                System.out.println("NSE of " + array[i] + " is " + stack.peek());
+            }
+            stack.push(array[i]);
+        }
     }
 }
